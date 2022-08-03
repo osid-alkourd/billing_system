06c475729f4495e242b47c3289f49ac9c45d36c1
Osid ALkourd <alkordosid73@gmail.com>
2022-08-03 12:07:04 -0700

    Add spatie Package for Authorization

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'locale' =>   \App\Http\Middleware\Localization::class
+        'locale' =>   \App\Http\Middleware\Localization::class ,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
